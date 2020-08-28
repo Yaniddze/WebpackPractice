2c686516e4aacba159d50f4280e5cbc2e5d9fc72
Yaniddze <FaceCrack1337@gmail.com>
2020-08-28 19:39:13 +0500

    Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.tsx',
+  mode: 'development',
+  devtool: 'inline-source-map',
+  resolve: {
+    extensions: ['.ts', '.tsx', 'js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.svg$/,
+        loader: 'svg-url-loader',
+      },
+    ]
+  },
+  output: {
+    filename: 'js/bundle.js',
+    path: path.resolve(__dirname, 'build'),
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './public.index.html',
+      filename: 'index.html',
+    })
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'build'),
+    compress: true,
+    part: 9000,
+  }
+}
