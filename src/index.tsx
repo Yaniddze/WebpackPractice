511d03201e71f97d79306630917949e5fd7153fc
Yaniddze <FaceCrack1337@gmail.com>
2020-08-29 09:37:17 +0500

    Add styled-components

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import styled from 'styled-components';
+
+type ButtonPropTypes = {
+  isBlack: boolean;
+}
+
+const Button = styled.button<ButtonPropTypes>`
+  background-color: ${props => props.isBlack? 'black': 'red'};
+`;
+
+const Wrapper = styled.div`
+  margin: 10px;
+`;
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <div>Hello!1122</div>
-  </React.StrictMode>,
+  <div>
+    <Wrapper>
+      <Button isBlack>
+        Cool
+      </Button>
+      <Button isBlack={false}>
+        Cool
+      </Button>
+    </Wrapper>
+  </div>,
   document.getElementById('root')
 );
